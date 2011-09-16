6fc7e822f3b922bba3a683df6cd51957fcd5524f
Wenzheng Jia <wenzheng.jia@gmail.com>
2011-09-16 14:47:00 -0500

    Pushing new changes

--- source/nothinbutdotnetstore.specs/WebFormDisplayEngineSpecs.cs
+++ source/nothinbutdotnetstore.specs/WebFormDisplayEngineSpecs.cs
@@ -1,5 +1,11 @@
-﻿using Machine.Specifications;
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using Machine.Specifications;
+using developwithpassion.specifications.extensions;
 using developwithpassion.specifications.rhinomocks;
+using nothinbutdotnetstore.specs.utility;
+using nothinbutdotnetstore.web.application;
 using nothinbutdotnetstore.web.core;
 using nothinbutdotnetstore.web.core.aspnet;
 
@@ -15,7 +21,33 @@ namespace nothinbutdotnetstore.specs
 
         public class when_displaying_a_report : concern
         {
-            It should_ = () => 
+            Establish c = ()=>
+            {
+                someData = new Object();
+
+                path = "somepage.aspx";
+                http_context = ObjectFactory.web.create_http_context();
+                depends.on(http_context);
+                depends.on(path);
+            };
+
+            Because b = () =>
+                sut.display(someData);
+
+            It should_set_the_data_in_context = () =>
+                http_context.Items["data"].ShouldNotBeNull();
+
+            It data_in_context_should_be_the_same_as_input_data = () =>
+                http_context.Items["data"].ShouldBeTheSameAs(someData);
+
+            //It should_transfer_to_a_page = () =>
+            //    http_context.Server.received(x => x.Transfer(path));
+
+            static System.Web.HttpContext http_context;
+            static object someData;
+            static IHttpHandler http_handler;
+            static string path;
         }
     }
+
 }
--- source/nothinbutdotnetstore/web/core/aspnet/WebFormDisplayEngine.cs
+++ source/nothinbutdotnetstore/web/core/aspnet/WebFormDisplayEngine.cs
@@ -1,10 +1,23 @@
-﻿namespace nothinbutdotnetstore.web.core.aspnet
+﻿using System.Web;
+
+namespace nothinbutdotnetstore.web.core.aspnet
 {
     public class WebFormDisplayEngine :IDisplayReports
     {
+        HttpContext http_context;
+        string target_page_url;
+
+        public WebFormDisplayEngine(HttpContext http_context, string target_page_url)
+        {
+            this.http_context = http_context;
+            this.target_page_url = target_page_url;
+        }
+
         public void display<ReportModel>(ReportModel model)
         {
-            throw new System.NotImplementedException();
+            http_context.Items.Add("data",model);
+            //http_context.Server.Transfer(target_page_url);
+
         }
     }
 }
